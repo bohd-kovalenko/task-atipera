01e5c118146a55ce4a0d5c15372bd35e0955f7b8
CoolTrol <bohdan1234yunior@gmail.com>
2023-08-25 14:42:14 +0300

    Initial commit

---
+++ src/test/java/com/atipera/taskatipera/TaskAtiperaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atipera.taskatipera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAtiperaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
